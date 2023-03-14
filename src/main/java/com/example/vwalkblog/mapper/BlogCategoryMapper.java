dc20a2c5afd864bceb4a2cd6982dcfe16eb316d1
DJM <3258074633@qq.com>
2023-03-14 10:10:18 +0800

    blog-category更新v2

---
+++ src/main/java/com/example/vwalkblog/mapper/BlogCategoryMapper.java
@@ -0,0 +1,18 @@
+package com.example.vwalkblog.mapper;
+
+import com.example.vwalkblog.pojo.BlogCategory;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 32580
+* @description 针对表【blog_category(blog对应的分类)】的数据库操作Mapper
+* @createDate 2023-03-14 09:08:17
+* @Entity com.example.vwalkblog.pojo.BlogCategory
+*/
+public interface BlogCategoryMapper extends BaseMapper<BlogCategory> {
+
+}
+
+
+
+
